293cf5ce0cfad53480e1f38d9f1ef618a8a4302b
Jim Blackler <jimblackler@google.com>
2020-10-03 14:25:52 +0100

    Proof of concept integration of the jsonschematypes library.

--- application/src/main/java/net/jimblackler/tryjsonschematypes/Example.java
+++ application/src/main/java/net/jimblackler/tryjsonschematypes/Example.java
@@ -1,17 +1,39 @@
 package net.jimblackler.tryjsonschematypes;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URI;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.jimblackler.jsonschemafriend.GenerationException;
+import net.jimblackler.jsonschemafriend.Schema;
+import net.jimblackler.jsonschemafriend.SchemaStore;
+import net.jimblackler.jsonschemafriend.Validator;
+import org.json.JSONObject;
 
 @WebServlet(name = "Example", value = "/example")
 public class Example extends HttpServlet {
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
     response.setContentType("text/html");
-    response.getWriter().println("<h1>Good afternoon. And good morning.</h1>");
+    PrintWriter writer = response.getWriter();
+    writer.println("<h1>Good afternoon. And good morning.</h1>");
+    SchemaStore schemaStore = new SchemaStore();
+    try {
+      Schema schema =
+          schemaStore.loadSchema(URI.create("https://json.schemastore.org/chrome-manifest"));
+      writer.println("<ul>");
+      JSONObject document = new JSONObject();
+      Validator.validate(
+          schema, document, error -> true, error -> writer.println("<li>" + error + " </li>"));
+      writer.println("</ul>");
+    } catch (GenerationException e) {
+      throw new ServletException(e);
+    }
   }
 
   public static String getInformation() {
